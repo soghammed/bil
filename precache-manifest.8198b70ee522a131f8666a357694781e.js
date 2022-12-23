7ecfbe8e8f51bd2d195538fb1cfcf50a8893eae8
Mohammed <devhammed@gmail.com>
2022-12-23 22:51:58 +0000

    Updates

---
+++ precache-manifest.8198b70ee522a131f8666a357694781e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b674c9556a70f218fbe8",
+    "url": "./static/css/main.b7199214.chunk.css"
+  },
+  {
+    "revision": "b674c9556a70f218fbe8",
+    "url": "./static/js/main.b674c955.chunk.js"
+  },
+  {
+    "revision": "dc2c6ecaabfff3523052",
+    "url": "./static/js/1.dc2c6eca.chunk.js"
+  },
+  {
+    "revision": "397f5d57c06adb836189",
+    "url": "./static/js/2.397f5d57.chunk.js"
+  },
+  {
+    "revision": "3b23b0e64e78688101da",
+    "url": "./static/js/runtime~main.3b23b0e6.js"
+  },
+  {
+    "revision": "fe79338a713d12350cfbe19e1b38443d",
+    "url": "./static/media/airbnblogo.fe79338a.png"
+  },
+  {
+    "revision": "90e811e4e68abd46e6fe2ab2e69167b4",
+    "url": "./static/media/maldives.90e811e4.jpg"
+  },
+  {
+    "revision": "8e98e259fd3216fe46628b6e35a17e10",
+    "url": "./static/media/background.8e98e259.jpg"
+  },
+  {
+    "revision": "e91768d75048f3fd14f9fc966b80467a",
+    "url": "./index.html"
+  }
+];
